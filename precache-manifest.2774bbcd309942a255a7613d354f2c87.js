bb11b4f514db10fa0b07015e95cb4a1b09334cd2
sebastiankut <sebastian_kut@yahoo.co.uk>
2020-09-06 21:25:05 +0200

    Updates

---
+++ precache-manifest.2774bbcd309942a255a7613d354f2c87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ba984cdb8abf6840b13fe78f03ca21",
+    "url": "/random_quote_generator/index.html"
+  },
+  {
+    "revision": "e610fd4c89b24635898e",
+    "url": "/random_quote_generator/static/css/main.7084dfb8.chunk.css"
+  },
+  {
+    "revision": "a0d33b5aadd8e6797899",
+    "url": "/random_quote_generator/static/js/2.6b2b8b0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random_quote_generator/static/js/2.6b2b8b0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e610fd4c89b24635898e",
+    "url": "/random_quote_generator/static/js/main.06dd0dfa.chunk.js"
+  },
+  {
+    "revision": "bf54d3b09a2aa3f6cf1a",
+    "url": "/random_quote_generator/static/js/runtime-main.1f9f6f5d.js"
+  }
+]);
